12890ae8f3321d15b6d41036a178e27b13d57640
rulifeng <rulifeng@meishubao.com>
2020-07-08 15:20:27 +0800

    视图与控制器逻辑

--- src/controller/home/food.js
+++ src/controller/home/food.js
@@ -3,6 +3,11 @@ const Base = require('../base');
 module.exports = class extends Base {
   async indexAction() {
     this.meta_title = '重庆美食';
+    const foodModel = this.model('food');
+    const foodList = await foodModel.getFoodList(1);
+    this.assign({
+      foodList
+    });
     return this.display();
   };
 
